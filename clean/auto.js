17e9e61beecd033950580e51fbfe41319fa53cac
wwi <3038600588@qq.com>
2023-04-28 01:00:40 +0800

    更新 auto.js
    删除空白行

--- clean/auto.js
+++ clean/auto.js
@@ -10,3 +10,25 @@ spread.bind(GC.Spread.Sheets.Events.ActiveSheetChanging, function (sender, args)
         b +=1
     }
 });
+
+// 获取当前表格
+var sheet = spread.getActiveSheet();
+
+// 获取表格的行数和列数
+var rowCount = sheet.getRowCount();
+var colCount = sheet.getColumnCount();
+
+// 从最后一行开始往上遍历，如果整行都是空白则删除该行
+for (var i = rowCount - 1; i >= 0; i--) {
+    var isBlank = true;
+    for (var j = 0; j < colCount; j++) {
+        var cellValue = sheet.getValue(i, j);
+        if (cellValue !== null && cellValue !== undefined && cellValue !== '') {
+            isBlank = false;
+            break;
+        }
+    }
+    if (isBlank) {
+        sheet.deleteRows(i, 1);
+    }
+}
